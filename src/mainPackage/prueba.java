371274fe08ffe26fdcd378cd6e58d3673cec8d29
jarbar <jarbar@hotmail.es>
2013-03-12 16:47:15 +0100

    prueba
    Signed-off-by: jarbar <jarbar@hotmail.es>

--- src/mainPackage/prueba.java
+++ src/mainPackage/prueba.java
@@ -1,14 +1,12 @@
 package mainPackage;
 
-public class Main {
+public class prueba {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hola caracola");
 
 	}
 
